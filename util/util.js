fab8e5fec609dcd535637458e037a27382f28ae3
Eedoh <ekabelly@gmail.com>
2021-03-23 18:56:08 +0200

    feat(util): added comments

--- util/util.js
+++ util/util.js
@@ -1,5 +1,6 @@
 const errorMessages = require("../constants/error-messages");
 
+// This function will wrap query.conn with promise and err handler  since conn.query doesn't return a promise
 const promiseWrap = cb =>
     new Promise((resolve, reject) =>
         cb((err, data, id) => {
@@ -12,6 +13,7 @@ const promiseWrap = cb =>
             resolve(data);
         }));
 
+// Create a date time for mySql
 const createNewDatetime = () => new Date().toISOString().slice(0, 19).replace('T', ' ')
 
 module.exports = {
